bb0a86cf28bbbbd72e4d4a462fbdddc640c372e4
Nathan Petrangelo <nathan@petrangelo.org>
2022-05-12 19:56:04 -0400

    Added Ballot and BallotBox types

---
+++ vote.ts
@@ -0,0 +1,22 @@
+class Ballot<Vote> {
+    vote: Vote
+    timestamp: number
+    constructor(vote) {
+        this.vote = vote;
+        this.timestamp = Date.now();
+    }
+}
+
+abstract class BallotBox<Vote> {
+    votes: Array<Ballot<Vote>>
+    getWinner: () => any;
+
+    merge(that) {
+        for (let i = 0; i < this.votes.length; i++) {
+            if (that.votes[i].timestamp > this.votes[i].timestamp) {
+                this.votes[i] = that.votes[i];
+            }
+        }
+    }
+}
+export default BallotBox;
