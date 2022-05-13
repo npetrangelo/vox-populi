179358146638b52d53c53ccd4b03856c53304fa5
Nathan Petrangelo <nathan@petrangelo.org>
2022-05-13 19:30:48 -0400

    First two unit tests pass

--- tests/unit-tests/BooleanBallotBox.test.ts
+++ tests/unit-tests/BooleanBallotBox.test.ts
@@ -12,11 +12,13 @@ describe("Testing BooleanBallotBox", () => {
       box.placeVote(0, true);
       box.placeVote(1, false);
       box.placeVote(2, false);
-      expect(0).toBe(0);
-      // console.log(foo);
+      expect(box.getWinner()).toBe(false);
    });
 
    it("returns true when there are enough votes", () => {
-
+      box.placeVote(0, true);
+      box.placeVote(1, true);
+      box.placeVote(2, false);
+      expect(box.getWinner()).toBe(true);
    });
 });
